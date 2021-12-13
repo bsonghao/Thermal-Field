3ab7745e1bd73a1c75257c9226b3e43ff72ea222
bsonghao <bsonghao@gmail.com>
2021-12-13 17:03:56 -0500

    clean up the code: first pass, change variable name avoid using "_" to name private variable

--- test_HF.py
+++ test_HF.py
@@ -4,7 +4,7 @@ import numpy as np
 from Hartree_Fock import check_integral
 
 # MO
-MO = True
+MO = False
 
 #geometry of molecules (in Angstrom)
 HF = 'H 0 0 0; F 0 0 1.1'
@@ -29,53 +29,53 @@ mf.kernel()
 
 # extract Hamiltonian parameters
 # 1-electron integral
-hcore_ao = mol_hf.intor('int1e_kin_sph') + mol_hf.intor('int1e_nuc_sph')
-hcore_mo = np.einsum('pi,pq,qj->ij', mf.mo_coeff, hcore_ao, mf.mo_coeff)
-print("1-electron integral (in AO basis):\n{:}".format(hcore_ao.shape))
-print("1-electron integral (in MO basis):\n{:}".format(hcore_mo.shape))
+hcoreAO = mol_hf.intor('int1e_kin_sph') + mol_hf.intor('int1e_nuc_sph')
+hcoreMO = np.einsum('pi,pq,qj->ij', mf.mo_coeff, hcoreAO, mf.mo_coeff)
+print("1-electron integral (in AO basis):\n{:}".format(hcoreAO.shape))
+print("1-electron integral (in MO basis):\n{:}".format(hcoreMO.shape))
 
 # overlap matrix
-S_ao = mol_hf.intor('int1e_ovlp_sph')
-S_mo = np.einsum('pi,pq,qj->ij', mf.mo_coeff, S_ao, mf.mo_coeff)
+SAO = mol_hf.intor('int1e_ovlp_sph')
+SMO = np.einsum('pi,pq,qj->ij', mf.mo_coeff, SAO, mf.mo_coeff)
 
 # 2-electron integral
-eri_ao = mol_hf.intor('int2e_sph', aosym=1)
-eri_mo = ao2mo.incore.full(eri_ao, mf.mo_coeff)
-print("2-electron integral (in AO basis):\n{:}".format(eri_ao.shape))
-print("2-electron integral (in MO basis):\n{:}".format(eri_mo.shape))
+eriAO = mol_hf.intor('int2e_sph', aosym=1)
+eriMO = ao2mo.incore.full(eriAO, mf.mo_coeff)
+print("2-electron integral (in AO basis):\n{:}".format(eriAO.shape))
+print("2-electron integral (in MO basis):\n{:}".format(eriAO.shape))
 
 # Fock matrix
-fock_ao = mf.get_fock()
-fock_mo = np.einsum('pi,pq,qj->ij', mf.mo_coeff, fock_ao, mf.mo_coeff)
-print("Fock matrix (in AO basis):\n{:}".format(fock_ao.shape))
-print("Fock matrix (in MO basis):\n{:}".format(fock_mo.shape))
+fockAO = mf.get_fock()
+fockMO = np.einsum('pi,pq,qj->ij', mf.mo_coeff, fockAO, mf.mo_coeff)
+print("Fock matrix (in AO basis):\n{:}".format(fockAO.shape))
+print("Fock matrix (in MO basis):\n{:}".format(fockMO.shape))
 
 # energy expectation value (HF energy)
-E_HF = mf.e_tot
+EHartreeFock = mf.e_tot
 
-print("energy expectation value (HF energy) (in Hartree):{:.5f}".format(E_HF))
+print("energy expectation value (HF energy) (in Hartree):{:.5f}".format(EHartreeFock))
 
 # total number of electrons
-occupation_number = mf.mo_occ / 2
-n_el = sum(occupation_number)
-print("total number of electrons: {:}".format(n_el))
-print("occupation number:\n{:}".format(occupation_number))
+OccupationNumber = mf.mo_occ / 2
+nelectron = sum(OccupationNumber)
+print("total number of electrons: {:}".format(nelectron))
+print("occupation number:\n{:}".format(OccupationNumber))
 
 # get NuclearRepulsionEnergy
 NRE = mf.energy_nuc()
 
 if MO:
-    hcore = hcore_mo
-    fock = fock_mo
-    eri = eri_mo
-    S = S_mo
+    hcore = hcoreMO
+    fock = fockMO
+    eri = eriMO
+    S = SMO
 else:
-    hcore = hcore_ao
-    fock = fock_ao
-    eri = eri_ao
-    S = S_ao
+    hcore = hcoreAO
+    fock = fockAO
+    eri = eriAO
+    S = SAO
 
 # run TFCC & thermal NOE calculation
-model = check_integral(E_HF, hcore, fock, eri, n_el, S, occupation_number, NRE, molecule=molecule, MO=MO)
+model = check_integral(EHartreeFock, hcore, fock, eri, nelectron, S, OccupationNumber, NRE, molecule=molecule, MO=MO)
 # thermal field transform
 model.my_SCF()
