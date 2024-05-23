9139b4e977fada50a33212fa184f88b09a80e773
bsonghao <bsonghao@gmail.con>
2024-05-23 18:35:39 -0400

    change the order of applying chemical potential and canonical constraint (after correcting the occupation number

--- test.py
+++ test.py
@@ -210,7 +210,7 @@ def main():
 
     # run TFCC & thermal NOE calculation
     model = two_body_model(molecule, E_core, h_core, Fock_ground_state, eri_integral, nof_electron, molecule=molecule,
-                       E_NN=NR_energy, T_2_flag=True, chemical_potential=False, partial_trace_condition=False)
+                       E_NN=NR_energy, T_2_flag=True, chemical_potential=True, partial_trace_condition=False)
     # thermal field transform
     model.thermal_field_transform(T=1e8)
     # TFCC imaginary time integration
